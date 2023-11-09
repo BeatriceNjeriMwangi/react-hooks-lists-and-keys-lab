5d06daf4437176dfff6460894eace736bb4c3c15
beatricenjerimwangi <bbeatricemwangi@gmail.com>
2023-11-09 13:32:13 -0500

    completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,17 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkElements = links.map((link, index) => {
+    console.log(index, link);
+    let url = `#${link}`
+    console.log(url);
+    return <a key={index} href={url}>{link}</a>
+  });
+
+  return <nav>
+    {linkElements}
+    
+    </nav>;
 }
 
 export default NavBar;
